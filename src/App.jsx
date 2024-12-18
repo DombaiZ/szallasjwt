65798692dd9c26b3f549605e273b8531f6d825a5
Dombai Zoltán <dombaiz@kkszki.hu>
2024-12-18 11:19:34 +0100

    Apróbb módosítás

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,26 @@ import './App.css';
 export const App = () => {
   return (
     <Router>
-      <nav className='navbar navbar-expand navbar-dark bg-dark'>
-        <ul className="navar-nav">
+      <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
+        <div className='container-fluid'>
+        <div className='navbar-brand'>SzallasJWT</div>
+        <button className='navbar-toggler' type='button' data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-expanded="false" aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id="navbarNav">
+        <ul className="navbar-nav">
           <li className='nav-item'>
-            <NavLink to={"/"}>Login</NavLink>
+            <NavLink to={"/"} className={({isActive}) => "nav-link" + (isActive ? " active" : "")}><span className='nav-link'>Login</span></NavLink>
           </li>
           <li className='nav-item'>
-            <NavLink to={"/SzallasList"}>SzallasList</NavLink>
+            <NavLink to={"/SzallasList"} className={({isActive}) => "nav-link" + (isActive ? " active" : "")}><span className='nav-link'>SzallasList</span></NavLink>
           </li>
           <li className='nav-item'>
-            <NavLink to={"/Logout"}>Kijelentkezés</NavLink>
+            <NavLink to={"/Logout"} className={({isActive}) => "nav-link" + (isActive ? " active" : "")}><span className='nav-link'>Kijelentkezés</span></NavLink>
           </li>
         </ul>
+        </div>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element = {<Login />} />
