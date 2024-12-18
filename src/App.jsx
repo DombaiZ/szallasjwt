b61541a4794fb90f8405dfcb83c864ec190e664c
Dombai Zoltán <dombaiz@kkszki.hu>
2024-12-18 10:41:30 +0100

    első commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink} from 'react-router-dom';
+import {Login} from './Login';
+import {SzallasList} from './SzallasList'; 
+import './App.css';
+
+export const App = () => {
+  return (
+    <Router>
+      <nav className='navbar navbar-expand navbar-dark bg-dark'>
+        <ul className="navar-nav">
+          <li className='nav-item'>
+            <NavLink to={"/"}>Login</NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink to={"/SzallasList"}>SzallasList</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element = {<Login />} />
+        <Route path="SzallasList" element = {<SzallasList />} />
+      </Routes>
+    </Router>
+  );
+}
